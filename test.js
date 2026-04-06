74dbbe29cb0b9334b50931e3cb4e8062240cdaa2
Roben-hou <coppinkausar@gmail.com>
2026-04-06 17:33:14 +0800

    initial

---
+++ test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let count;
+count = function (a, b) {
+    return a + b;
+};
+console.log(count(1, 2));
+//# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
-let namee: string = 'lzp';
+let count: (a: number, b: number) => number
+
+count = function (a, b) {
+    return a + b;
+}
+
+console.log(count(1, 2));
