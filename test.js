fee976eed0ed3a97485d5edda3ab1a8ce1ef998f
Roben-hou <coppinkausar@gmail.com>
2026-04-06 20:20:02 +0800

    enum

--- test.js
+++ test.js
@@ -1,12 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let arr1;
-let arr2;
-let arr3;
-arr1 = [1, 'hello'];
-arr2 = [11, true];
-arr3 = ['hi', 1, 2, 3, 4, 5, 6];
-console.log(arr1);
-console.log(arr2);
-console.log(arr3);
+var Direction;
+(function (Direction) {
+    Direction[Direction["up"] = 0] = "up";
+    Direction[Direction["down"] = 1] = "down";
+    Direction[Direction["left"] = 2] = "left";
+    Direction[Direction["right"] = 3] = "right";
+})(Direction || (Direction = {}));
+function walk(data) {
+    if (data === Direction.up) {
+        console.log("向上走");
+    }
+    else if (data === Direction.down) {
+        console.log("向下走");
+    }
+    else if (data === Direction.left) {
+        console.log("向左走");
+    }
+    else if (data === Direction.right) {
+        console.log("向右走");
+    }
+}
+walk(Direction.up);
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,11 +1,24 @@
-let arr1: [number, string]
-let arr2: [number, boolean]
-let arr3: [string, ...number[]]
-
-
-arr1 = [1, 'hello']
-arr2 = [11, true]
-arr3 = ['hi', 1, 2, 3, 4, 5, 6]
-console.log(arr1)
-console.log(arr2)
-console.log(arr3)
+enum Direction {
+    up,
+    down,
+    left,
+    right
+}
+function walk(data: Direction) {
+    if (data === Direction.up) {
+        console.log("向上走")
+    }
+    else if (data === Direction.down) {
+        console.log("向下走")
+    }
+    else if (data === Direction.left) {
+        console.log("向左走")
+    }
+    else if (data === Direction.right) {
+        console.log("向右走")
+    }
+    else {
+        console.log("方向错误")
+    }
+}
+walk(Direction.up)
