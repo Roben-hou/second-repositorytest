e4c0dba124c10d5e91f35e7fd8166aced5aafba9
Roben-hou <coppinkausar@gmail.com>
2026-04-06 18:11:42 +0800

    tuple

--- test.js
+++ test.js
@@ -2,8 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let arr1;
 let arr2;
-arr1 = [1, 2];
-arr2 = ['aaa', 'bbb'];
+let arr3;
+arr1 = [1, 'hello'];
+arr2 = [11, true];
+arr3 = ['hi', 1, 2, 3, 4, 5, 6];
 console.log(arr1);
 console.log(arr2);
+console.log(arr3);
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,7 +1,11 @@
-let arr1: number[]
-let arr2: Array<string>
+let arr1: [number, string]
+let arr2: [number, boolean]
+let arr3: [string, ...number[]]
 
-arr1 = [1, 2]
-arr2 = ['aaa', 'bbb']
+
+arr1 = [1, 'hello']
+arr2 = [11, true]
+arr3 = ['hi', 1, 2, 3, 4, 5, 6]
 console.log(arr1)
 console.log(arr2)
+console.log(arr3)
